0be4412818c99bb6462433ee138799115bb6527e
Wan Mohd Fardlullah <wmfwmn1991@gmail.com>
2026-05-07 13:48:48 +0800

    update frontend api to production backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/scam",
+    baseURL: "https://scam-detector-fullstack-assignment-3-2.onrender.com/api/scam",
 });
 
 export default api;
